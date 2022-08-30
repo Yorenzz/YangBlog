5e31057983f663050c79ef3f1ed732d7398ba05a
yangenze <yangenze@4399.com>
2022-08-30 10:14:55 +0800

    api更改

--- YangWebBlog/src/request/index.js
+++ YangWebBlog/src/request/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const request=axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'http://175.178.115.221:8080/',
     timeout:30000
 })
 export default request

bca4324b1a230134d5d2fad7fb5be0d9ee815b44
yangenze <yangenze@4399.com>
2022-08-31 09:44:29 +0800

    api change

--- YangWebBlog/src/config/index.js
+++ YangWebBlog/src/config/index.js
@@ -1,7 +1,8 @@
 const env = import.meta.env.MODE || 'prod'
 const EnvConfig = {
   dev: {
-    baseApi: 'http://localhost:8080',
+    baseApi: 'http://175.178.115.221:8080',
+    // baseApi: 'http://localhost:8080',
   },
   prod: {
     baseApi: 'http://175.178.115.221:8080',
